ceda7a7365f99488257aad0172615c6991d2bfa4
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-09-28 20:36:17 +0530

    server

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express=require("express")
+const  cors=require("cors")
 const { connection } = require("./db")
 const { userRoute } = require("./Routes/userRoutes")
 const { blogRoutes } = require("./Routes/blogRoutes")
-const cors =require("cors")
+
 const app=express()
 app.use(express.json())
 app.use(cors())
