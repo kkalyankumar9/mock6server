d748335153720282b875f7d0e957a17f4eb04223
kkalyankumar9 <kummarikalyan010@gmail.com>
2023-09-28 20:05:25 +0530

    server

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 const mongoose=require("mongoose")
 
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/mock6")
+const connection=mongoose.connect("mongodb+srv://kkalyan:kalyan@cluster0.iisergf.mongodb.net/mock6?retryWrites=true&w=majority")
 module.exports={connection}
